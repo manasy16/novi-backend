b43363935bd8281df2df63a0a156bc94546bdf9e
iabhiijeet <abhijeetrao7800@gmail.com>
2026-09-15 20:43:47 +0530

    Add CORS middleware to FastAPI application

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.auth.routes import router as auth_router
 from app.api.module1_routes import router as module1_router
@@ -7,6 +8,19 @@ from app.api.module1_routes import router as module1_router
 app = FastAPI(
     title="NOVI API",
     version="1.0.0",
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:3000",
+        "http://localhost:3001",
+        "http://127.0.0.1:3000",
+        "http://127.0.0.1:3001",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(auth_router)
